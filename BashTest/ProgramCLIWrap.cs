67085bf911b0e7d7851c91f4685692de12b8f65a
Dave Mateer <davemateer@gmail.com>
2021-06-21 12:28:06 +0100

    gitignore

--- BashTest/ProgramCLIWrap.cs
+++ BashTest/ProgramCLIWrap.cs
@@ -8,12 +8,10 @@ namespace BashTest
 {
     class Program
     {
+        // Run this from WSL2 side as I'm expecting to use bash
+        // dotnet run
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World from C#!");
-            Console.WriteLine($"AppDomain.CurrentDomain.BaseDirectory is {AppDomain.CurrentDomain.BaseDirectory}");
-            Console.WriteLine($"Environment.CurrentDirectory is {Environment.CurrentDirectory}");
-
             // 1.Task based 
             //var result = await Cli.Wrap("infra.azcli")
             //                .WithWorkingDirectory(workingDir)
@@ -24,32 +22,47 @@ namespace BashTest
             // don't need to call .ExecuteAsync as we are calling ListenAsync below
             var cmd = Cli.Wrap("infra.azcli");
 
+            // Can pass a cancellation token
             using var cts = new CancellationTokenSource();
 
             // Cancel automatically after a timeout of 10 seconds
-            cts.CancelAfter(TimeSpan.FromSeconds(10));
+            //cts.CancelAfter(TimeSpan.FromSeconds(10));
 
-            await foreach (var cmdEvent in cmd.ListenAsync(cts.Token))
+            // Cli will throw if a non 0 return status
+            try
             {
-                switch (cmdEvent)
+                await foreach (var cmdEvent in cmd.ListenAsync(cts.Token))
                 {
-                    case StartedCommandEvent started:
-                        //_output.WriteLine($"Process started; ID: {started.ProcessId}");
-                        Console.WriteLine($"Process started; ID: {started.ProcessId}");
-                        break;
-                    case StandardOutputCommandEvent stdOut:
-                        //_output.WriteLine($"Out> {stdOut.Text}");
-                        Console.WriteLine($"Out> {stdOut.Text}");
-                        break;
-                    case StandardErrorCommandEvent stdErr:
-                        //_output.WriteLine($"Err> {stdErr.Text}");
-                        Console.WriteLine($"Err> {stdErr.Text}");
-                        break;
-                    case ExitedCommandEvent exited:
-                        //_output.WriteLine($"Process exited; Code: {exited.ExitCode}");
-                        Console.WriteLine($"Process exited; Code: {exited.ExitCode}");
-                        break;
+                    switch (cmdEvent)
+                    {
+                        case StartedCommandEvent started:
+                            //_output.WriteLine($"Process started; ID: {started.ProcessId}");
+                            Console.WriteLine($"Process started; ID: {started.ProcessId}");
+                            break;
+                        case StandardOutputCommandEvent stdOut:
+                            //_output.WriteLine($"Out> {stdOut.Text}");
+                            Console.WriteLine($"Out> {stdOut.Text}");
+                            break;
+                        case StandardErrorCommandEvent stdErr:
+                            //_output.WriteLine($"Err> {stdErr.Text}");
+                            Console.WriteLine($"Err> {stdErr.Text}");
+                            // it could be that we want to exit here if any of the script writes to stderr
+                            // make sure script debugging turned off ie no set -x
+                            break;
+                        case ExitedCommandEvent exited:
+                            //_output.WriteLine($"Process exited; Code: {exited.ExitCode}");
+                            Console.WriteLine($"Process exited; Code: {exited.ExitCode}");
+                            break;
+                    }
                 }
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine("Operation cancelled - can I recover / clean up?");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception - this can be caused when the bash command returns a non 0 status");
             }
 
             //Console.WriteLine($"Result: {result}");
